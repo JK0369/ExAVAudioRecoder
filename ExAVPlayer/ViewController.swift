f1339677c417d44236b12175af0451407ca48c2e
jake.k <jake.k@hpcnt.com>
2022-04-28 16:00:06 +0900

    Initial Commit

---
+++ ExAVPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExAVPlayer
+//
+//  Created by Jake.K on 2022/04/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
